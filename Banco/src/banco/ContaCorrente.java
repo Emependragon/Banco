cfc667166344db19ad39bcec144fd06662a35de1
Emerson Da Flora Victor <127784324+Emependragon@users.noreply.github.com>
2023-03-31 00:13:27 +0200

    New

--- Banco/src/banco/ContaCorrente.java
+++ Banco/src/banco/ContaCorrente.java
@@ -8,10 +8,17 @@ package banco;
 public class ContaCorrente extends ContaBancaria {
 private double taxaDeOperacao;
 
-    public ContaCorrente(double taxaDeOperacao, String numeroConta, double saldo) {
+    public ContaCorrente(double taxaDeOperacao, String numeroConta, double saldo, Object par3) {
         super(numeroConta, saldo);
         this.taxaDeOperacao = taxaDeOperacao;
     }
+
+    ContaCorrente() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+
+
  @Override
     public void sacar(double valor) {
     double taxaDeOpecaracao = 0;
@@ -31,6 +38,14 @@ private double taxaDeOperacao;
       saldo += valorComTaxa;
        historico.add("Deposito: " +valorComTaxa);
     }  
+
+    void setNumeroDeConta(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void mostrarDados() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
    }
 
 
